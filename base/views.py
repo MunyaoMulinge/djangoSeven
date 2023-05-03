9a3f633cc9cb3ced855713e55da475aec9b46d60
MunyaoMulinge <munyaomulinge@protonmail.com>
2023-05-03 06:00:48 +0300

    count

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -11,11 +12,17 @@ from .forms import RoomForm
 # ]
 
 def home(request):
-    q = request.GET.get('q') if request.GET.get('q')!= None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
 
     topic = Topic.objects.all()
-    context = {'rooms': rooms, 'topics': topic}
+    room_count = rooms.count()
+
+    context = {'rooms': rooms, 'topics': topic, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 
